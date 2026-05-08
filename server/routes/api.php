e5f1f65d91b889be906de62a5807e3293aae793b
Johanna Hofer <s2310456005@fhooe.at>
2026-05-08 21:24:04 +0200

    Added Auth-Routes for api.php

--- server/routes/api.php
+++ server/routes/api.php
@@ -20,10 +20,6 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
-    return $request->user();
-});
-
 //--------------------------------------------------------------------------
 // Routes for courses
 //--------------------------------------------------------------------------
