263df10355b3c9b59b0d846122c7a9549c2737d7
Johanna Hofer <s2310456005@fhooe.at>
2026-05-24 22:36:54 +0200

    home page adjusted

--- client/fitnessapp26/src/app/pages/home/home.ts
+++ client/fitnessapp26/src/app/pages/home/home.ts
@@ -1,6 +1,7 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
 import { RouterLink } from '@angular/router';
 import { MatButton } from '@angular/material/button';
+import { Authentication } from '../../shared/services/authentication';
 
 @Component({
   selector: 'fa-home',
@@ -9,4 +10,29 @@ import { MatButton } from '@angular/material/button';
   templateUrl: './home.html',
   styleUrl: './home.scss'
 })
-export class Home {}
+export class Home {
+  // Use the authentication service to check whether a user is logged in
+  // and whether the user is a trainer or participant
+  private authService = inject(Authentication);
+
+  /**
+   * Return whether a user is currently logged in.
+   */
+  protected isLoggedIn(): boolean {
+    return this.authService.isLoggedIn();
+  }
+
+  /**
+   * Return whether the logged-in user is a trainer.
+   */
+  protected isTrainer(): boolean {
+    return this.authService.isLoggedIn() && this.authService.isTrainer();
+  }
+
+  /**
+   * Return whether the logged-in user is a participant.
+   */
+  protected isParticipant(): boolean {
+    return this.authService.isLoggedIn() && !this.authService.isTrainer();
+  }
+}
